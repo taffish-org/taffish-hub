6b5c23e0a0982971f4b1062a42330192effd3977
Kaiyuan Han <hermit_han@163.com>
2025-03-05 12:14:57 +0800

    change apps-list format to let cl's read can load

--- apps.lisp
+++ apps.lisp
@@ -1,4 +1,4 @@
-(("blast" "HermitHan/blast")
- ("cdhit" "HermitHan/cdhit")
- ("hmmer" "HermitHan/hmmer")
- ("get-seqs-from-ids" "HermitHan/get-seqs-from-ids"))
+("blast" "HermitHan/blast")
+("cdhit" "HermitHan/cdhit")
+("hmmer" "HermitHan/hmmer")
+("get-seqs-from-ids" "HermitHan/get-seqs-from-ids")
