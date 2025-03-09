87cedf6c8c60e651e3749b73d28ba4f329279bfc
Kaiyuan Han <hermit_han@163.com>
2025-03-10 01:37:37 +0800

    Update apps.lisp

--- apps.lisp
+++ apps.lisp
@@ -1,6 +1,7 @@
 ("debian"             "taffish-org/debian")
 ("python"             "taffish-org/python")
 ("sbcl"               "taffish-org/sbcl")
+("R"                  "taffish-org/R")
 
 ("blast"              "taffish-org/blast")
 ("cdhit"              "taffish-org/cdhit")
@@ -8,10 +9,11 @@
 ("get-seqs-from-ids"  "taffish-org/get-seqs-from-ids")
 ("gene-family-search" "taffish-org/gene-family-search")
 
-("sra-tools"   "taffish-org/sra-tools")
-("seqtk"       "taffish-org/seqtk")
-("fastqc"      "taffish-org/fastqc")
-("trim_galore" "taffish-org/trim_galore")
-("STAR"        "taffish-org/STAR")
+("sra-tools"          "taffish-org/sra-tools")
+("seqtk"              "taffish-org/seqtk")
+("fastqc"             "taffish-org/fastqc")
+("trim_galore"        "taffish-org/trim_galore")
+("STAR"               "taffish-org/STAR")
+("subread"            "taffish-org/subread")
 
-("muscle" "taffish-org/muscle")
+("muscle"             "taffish-org/muscle")
