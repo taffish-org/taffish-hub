7d1271394c1fd55674bf94ab941e1e7b85ac282b
Kaiyuan Han <hermit_han@163.com>
2025-03-12 03:53:04 +0800

    Update apps.lisp

--- apps.lisp
+++ apps.lisp
@@ -1,28 +1,35 @@
+;;BASE
 ("debian"             "taffish-org/debian")
 ("python"             "taffish-org/python")
 ("sbcl"               "taffish-org/sbcl")
 ("R"                  "taffish-org/R")
 
+;;TOOLs
 ("muscle"             "taffish-org/muscle")
 ("MCScanX"            "taffish-org/MCScanX")
 ("uniprot-idmapping"  "taffish-org/uniprot-idmapping")
 ("juicer"             "taffish-org/juicer")
 
+;;;gene-family-search
 ("blast"              "taffish-org/blast")
 ("cdhit"              "taffish-org/cdhit")
 ("hmmer"              "taffish-org/hmmer")
 ("get-seqs-from-ids"  "taffish-org/get-seqs-from-ids")
 ("gene-family-search" "taffish-org/gene-family-search")
 
+;;;RNA-seq
 ("sra-tools"          "taffish-org/sra-tools")
 ("seqtk"              "taffish-org/seqtk")
 ("fastqc"             "taffish-org/fastqc")
 ("trim_galore"        "taffish-org/trim_galore")
 ("STAR"               "taffish-org/STAR")
 ("subread"            "taffish-org/subread")
-;;("rna-seq")
 
+;;;T2T
 ("bedtools"           "taffish-org/bedtools")
 ("samtools"           "taffish-org/samtools")
 ("bowtie2"            "taffish-org/bowtie2")
 ("fastp"              "taffish-org/fastp")
+
+;;;single cell
+("kallisto"           "taffish-org/kallisto")
