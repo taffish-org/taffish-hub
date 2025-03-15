a92f7c9011f1a744cc4282787cb1e164a1d54c4d
Kaiyuan Han <hermit_han@163.com>
2025-03-15 21:41:56 +0800

    Create all-apps.lisp

---
+++ all-apps.lisp
@@ -0,0 +1,180 @@
+("debian" "taffish-org/debian"
+ "de7d99272d27a8e30b5b01889ac5c06889b41ae1	refs/tags/v12" "404: Not Found")
+("python" "taffish-org/python"
+ "7ee0abc69392bf88bf93fc28b7e1845d05ba7bbe	refs/tags/v3.13.2" "404: Not Found")
+("sbcl" "taffish-org/sbcl"
+ "e0c768f956afcb509e44410bf5f8de82aeb6a501	refs/tags/v2.5.2" "404: Not Found")
+("R" "taffish-org/R"
+ "1343743f111871dae8e244df555036e5670f6727	refs/tags/v4.4.3" "404: Not Found")
+("muscle" "taffish-org/muscle"
+ "814ff2be0abea6b8d64ceb94bf8156b9e6d9e299	refs/tags/v5.3" "# taf-muscle
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/rcedgar/muscle
+
+Muscle is widely-used software for making multiple alignments of biological sequences. Multiple sequence and structure alignment with top benchmark scores scalable to thousands of sequences. Generates replicate alignments, enabling assessment of downstream analyses such as trees and predicted structures.")
+("MCScanX" "taffish-org/MCScanX"
+ "9a615cd0779d911695c0bea4e48ad07d3a776831	refs/tags/v1.0.0" "# taf-MCScanX
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/wyp1125/MCScanX
+
+MCScanX: Multiple Collinearity Scan toolkit X version. The most popular synteny analysis tool in the world!")
+("uniprot-idmapping" "taffish-org/uniprot-idmapping"
+ "e1601e464357a8bde79a1b54339e21a73950ad86	refs/tags/v1.0.0"
+ "# taf-uniprot-idmapping
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is made by ShishiYuan
+
+A tool for connecting Uniprot.")
+("juicer" "taffish-org/juicer"
+ "490af26e54d42bf065a8c35fd82a3b92177b5916	refs/tags/v2.20.00" "# taf-juicer
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/aidenlab/juicer
+
+A One-Click System for Analyzing Loop-Resolution Hi-C Experiments.")
+("blast" "taffish-org/blast"
+ "fb4bb925d4d1404bd1c3f0d6b1698832a7b0972e	refs/tags/v2.16.0" "# taf-blast
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from docker.io/ncbi/blast(https://blast.ncbi.nlm.nih.gov/Blast.cgi).
+
+BLAST finds regions of similarity between biological sequences. The program compares nucleotide or protein sequences to sequence databases and calculates the statistical significance.")
+("cdhit" "taffish-org/cdhit"
+ "2a39ddc83105c66e4f258eebc6fec343d3b9a297	refs/tags/v4.8.1" "# taf-cdhit
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is made from github: https://github.com/weizhongli/cdhit
+
+CD-HIT is a very widely used program for clustering and comparing protein or nucleotide sequences")
+("hmmer" "taffish-org/hmmer"
+ "7e93db958148da4f292c83fdff1410ed764ca387	refs/tags/v3.4" "# taf-hmmer
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This is made by github: https://github.com/EddyRivasLab/hmmer
+
+HMMER is used for searching sequence databases for sequence homologs, and for making sequence alignments. It implements methods using probabilistic models called profile hidden Markov models (profile HMMs).")
+("get-seqs-from-ids" "taffish-org/get-seqs-from-ids"
+ "34dec31f6b8552e50b2105d6caff96dce4a9595f	refs/tags/v1.0.0"
+ "# taf-get-seqs-from-ids
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This taf-app is written by common lisp(with package hanky-lisp: https://github.com/HermitHan/hanky-lisp) with sbcl:2.5.0(docker.io/fukamachi/sbcl:2.5.0)
+
+Get-Seqs-From-IDs: this app use \"common lisp\"(need hanky-lisp package) to search ids(from a txt file which have line by line ids) from a fasta file(ids in '> ...' line), and format the ids' fasta seqs(or save to an output file)")
+("gene-family-search" "taffish-org/gene-family-search"
+ "8f2a737e9fb72716bf207662ca26cf21024b7c17	refs/tags/v1.0.0"
+ "# taf-gene-family-search
+
+- This is a taf-app(taf-flow), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is made by these taf-apps(taf-tools):
+  - taf-cdhit :: https://github.com/HermitHan/cdhit
+  - taf-hmmer :: https://github.com/HermitHan/hmmer
+  - taf-blast :: https://github.com/HermitHan/blast
+  - taf-get-seqs-from-ids :: https://github.com/HermitHan/get-seqs-from-ids
+
+This taf-app can search the gene-family from your input: hmm(.hmm) and blast(.fasta) db.")
+("sra-tools" "taffish-org/sra-tools"
+ "c522cc53eeb901bd0e272bff120477ac8635e00b	refs/tags/v3.2.0" "# taf-sra-tools
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/ncbi/sra-tools
+
+The SRA Toolkit and SDK from NCBI is a collection of tools and libraries for using data in the INSDC Sequence Read Archives.")
+("seqtk" "taffish-org/seqtk"
+ "98899dd97c6c8c7d064c24cc77319548df266f92	refs/tags/v1.4" "# taf-seqtk
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This is made by github: https://github.com/lh3/seqtk
+
+Seqtk is a fast and lightweight tool for processing sequences in the FASTA or FASTQ format. It seamlessly parses both FASTA and FASTQ files which can also be optionally compressed by gzip.")
+("fastqc" "taffish-org/fastqc"
+ "10e4617652945737e197e34f2953bbef334aaf8c	refs/tags/v0.11.9" "# taf-fastqc
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from debian12:(apt install fastqc) (https://github.com/s-andrews/FastQC)
+
+A quality control analysis tool for high throughput sequencing data.")
+("trim_galore" "taffish-org/trim_galore"
+ "492baf60e05788bed1cbe7aa79eafa702dc483e4	refs/tags/v0.6.10" "# taf-trim_galore
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is made from github: https://github.com/FelixKrueger/TrimGalore
+
+A wrapper around Cutadapt and FastQC to consistently apply adapter and quality trimming to FastQ files, with extra functionality for RRBS data.")
+("STAR" "taffish-org/STAR"
+ "4ea1ebb56d82f7af02e85472e5e92c7f1207a7fd	refs/tags/v2.7.11b" "# taf-STAR
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is made from github: https://github.com/alexdobin/STAR
+
+RNA-seq aligner.")
+("subread" "taffish-org/subread"
+ "71fcee1906074e91dfc02af99e0badeff68a7727	refs/tags/v2.0.6" "# taf-subread
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is made from github: https://github.com/ShiLab-Bioinformatics/subread
+
+The Subread software package is a tool kit for processing next-gen sequencing data. It includes Subread aligner, Subjunc exon-exon junction detector and featureCounts read summarization program.")
+("bedtools" "taffish-org/bedtools"
+ "024eed316694ad68e2ce2713579b198f7276c38e	refs/tags/v2.31.1" "# taf-bedtools
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/arq5x/bedtools2
+
+bedtools - the swiss army knife for genome arithmetic")
+("samtools" "taffish-org/samtools"
+ "429ffa924361466c2588b12cd9aa7916f58b8d94	refs/tags/v1.21" "# taf-samtools
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/samtools/samtools
+
+Tools (written in C using htslib) for manipulating next-generation sequencing data.")
+("bowtie2" "taffish-org/bowtie2"
+ "db33c9723668ef76f21a43b233fcdc30a254a666	refs/tags/v2.5.4" "# taf-bowtie2
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/BenLangmead/bowtie2
+
+A fast and sensitive gapped read aligner.")
+("fastp" "taffish-org/fastp"
+ "b02b1a8a611de9da4f3172e8040faac0a990c823	refs/tags/v0.24.0" "# taf-fastp
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/OpenGene/fastp
+
+An ultra-fast all-in-one FASTQ preprocessor (QC/adapters/trimming/filtering/splitting/merging...).")
+("kallisto" "taffish-org/kallisto"
+ "9aef9d6defc3a58169ec42433a9b6700c0dc8f88	refs/tags/v0.51.1" "# taf-kallisto
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/pachterlab/kallisto
+
+Near-optimal RNA-Seq quantification.")
+("bustools" "taffish-org/bustools"
+ "e19712c0d6f997e97eb6efd2359d56653cd8a871	refs/tags/v0.44.1" "# taf-bustools
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/BUStools/bustools
+
+Tools for working with BUS files.")
+("hisat2" "taffish-org/hisat2"
+ "0004d29c4b895e18496632630041163b1f429dbf	refs/tags/v2.2.1" "# taf-hisat2
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/DaehwanKimLab/hisat2
+
+Graph-based alignment (Hierarchical Graph FM index)
+
+(For now, this app is only for Linux-AMD64, can not use on ARM64)")
+("mafft" "taffish-org/mafft"
+ "37981e088534726cef4c5dcfaa2cfa7f36bcaf0a	refs/tags/v7.525" "# taf-mafft
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://mafft.cbrc.jp/alignment/software/source.html
+
+Multiple alignment program for amino acid or nucleotide sequences.")
+("clustal-omega" "taffish-org/clustal-omega"
+ "aba647ee26cf9932c7e5ad8af6029b8d2b235a21	refs/tags/v1.2.4" "")
